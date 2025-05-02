e24ffcffb9f2f085c57a8d1af2d8060e5806deb6
vallabh <vallabhjagtap39@gmail.com>
2025-05-02 13:12:42 +0530

    connected to db!

---
+++ src/index.js
@@ -0,0 +1,59 @@
+
+//require('dotenv').config({path: './env'})
+import dotenv from "dotenv"
+import mongoose, { connect } from "mongoose";
+
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path:'./env'
+})
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const app = express();
+
+
+// (async () => {
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//         app.on("error", (error ) => {
+//             console.log("Error : ", error);
+//             throw error
+//         })
+
+//         app.listen(process.env.PORT, () => {
+//             console.log(`app is listing on port ${process.env.PORT}`)
+//         });
+
+
+//     } catch (error) {
+//         console.log("Error : ",error);
+        
+//     }
+// })()
+
